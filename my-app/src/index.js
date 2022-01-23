c250250e08f3018f6db550b9bcc20a3f6f0d50ef
宮本貴史 <>
2022-01-23 19:35:11 +0900

    UIの状態を取得

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -7,22 +7,35 @@ import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
+import Switch from '@mui/material/Switch';
 
-const tick = ()=>{
+let uiData = {
+  time:new Date().toLocaleTimeString(),
+  switchStatus: false
+}
+const tick = () => {
   const element = (
     <div>
       <Container maxWidth="sm">
-      <Button variant="contained" color="primary" onClick={() => {
-    tick();
-  }}>
-      What time is it now?
-    </Button>
-    <Box sx={{ my: 4 }}>
+        <Button variant="contained" color="primary" onClick={() => {
+          uiData.time = new Date().toLocaleTimeString();
+          tick();
+        }}>
+          What time is it now?
+        </Button>
+        <Box sx={{ my: 4 }}>
+          <Typography variant="h4" component="h1" gutterBottom>
+            {uiData.time}
+          </Typography>
+        </Box>
+        <Switch onClick={(event) => { 
+          uiData.switchStatus = event.target.checked;
+          tick();
+        }} />
         <Typography variant="h4" component="h1" gutterBottom>
-        {new Date().toLocaleTimeString()}
-        </Typography>
-      </Box>
-    </Container>
+            {String( uiData.switchStatus)}
+          </Typography>
+      </Container>
     </div>
   );
   ReactDOM.render(element, document.getElementById('root'));
