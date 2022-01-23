407b7e1bb0c40c37b979e41fffdedae55b1ee7f9
宮本貴史 <>
2022-01-23 17:22:13 +0900

    clickで現在時刻取得

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+
+const tick = ()=>{
+  const element = (
+    <div>
+      <Container maxWidth="sm">
+      <Button variant="contained" color="primary" onClick={() => {
+    tick();
+  }}>
+      What time is it now?
+    </Button>
+    <Box sx={{ my: 4 }}>
+        <Typography variant="h4" component="h1" gutterBottom>
+        {new Date().toLocaleTimeString()}
+        </Typography>
+      </Box>
+    </Container>
+    </div>
+  );
+  ReactDOM.render(element, document.getElementById('root'));
+}
+
+tick();
+//setInterval(tick, 1000);
+
